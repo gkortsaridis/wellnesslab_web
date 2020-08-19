58cb8ba4dc6db97669d84eaec828d640c9652e8b
George Kortsaridis <gkortsaridis@gmail.com>
2020-08-19 19:10:56 +0100

    Updates

---
+++ precache-manifest.9720fc361ab805d36d2caa02986f5996.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7e18c8bf72c2decede142a97c9e67d0",
+    "url": "/wellnesslab_web/index.html"
+  },
+  {
+    "revision": "0dbebc4e96ada00786cb",
+    "url": "/wellnesslab_web/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fdf91845c2d83890febb",
+    "url": "/wellnesslab_web/static/js/2.7b0c7914.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wellnesslab_web/static/js/2.7b0c7914.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dbebc4e96ada00786cb",
+    "url": "/wellnesslab_web/static/js/main.67603f67.chunk.js"
+  },
+  {
+    "revision": "657ba9476c4c020f72a2",
+    "url": "/wellnesslab_web/static/js/runtime-main.17356633.js"
+  },
+  {
+    "revision": "19ef0f29f358307e6829fe4bef576a02",
+    "url": "/wellnesslab_web/static/media/anastasia_aivatoglou.19ef0f29.png"
+  },
+  {
+    "revision": "5745fdf1705ad7a03956e1111e609968",
+    "url": "/wellnesslab_web/static/media/chrysoula_grigoropoulou.5745fdf1.png"
+  },
+  {
+    "revision": "f63eca4e10d30cd759bd81ff8600bf04",
+    "url": "/wellnesslab_web/static/media/cover.f63eca4e.jpg"
+  },
+  {
+    "revision": "c1bb3e3a0f93a76a24771cfe5230700b",
+    "url": "/wellnesslab_web/static/media/email.c1bb3e3a.png"
+  },
+  {
+    "revision": "c5ddecc677d10dd4ae1a9216bf75dff7",
+    "url": "/wellnesslab_web/static/media/instagram.c5ddecc6.png"
+  },
+  {
+    "revision": "3955c15bc73afc4b72f5d0d8bd4425f8",
+    "url": "/wellnesslab_web/static/media/twitter.3955c15b.png"
+  }
+]);
