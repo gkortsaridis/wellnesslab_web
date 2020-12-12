23574c21b6b19f4f5222137c2a5a015b0d1720f7
George Kortsaridis <gkortsaridis@gmail.com>
2020-12-12 13:41:45 +0000

    Created Single Component for old UI

---
+++ src/Components/WLToolbar/WLToolbar.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class WLToolbar extends React.Component {
+
+    render() {
+        return (
+            <div style={this.styles.container}>
+
+            </div>
+        )
+    }
+
+    styles = {
+        container: {
+            height: "200px",
+            backgroundColor: 'red'
+        }
+    }
+}
+
+export default WLToolbar
---
+++ src/Entities.ts
@@ -0,0 +1,4 @@
+// @ts-ignore
+interface Article { title: string, imgUrl: string, articleUrl: string }
+interface SocialMedia { name: string, url: string }
+interface TeamMember { name: string, image: string, title: string, social: SocialMedia[]}
